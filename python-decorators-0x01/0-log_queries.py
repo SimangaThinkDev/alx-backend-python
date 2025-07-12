5d4d5eef92306c8a8bd933fa1f94dd33616c4c0f
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-07-12 14:21:48 +0200

    Done with log queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,32 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+
+def log_queries(func):
+    """This decorator logs query results."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print("-------------------------Logging Starts-------------------------")
+        result = func(*args, **kwargs)
+        for row in result:
+            print(row)
+        print("-------------------------Logging Ends-------------------------")
+        return result
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
