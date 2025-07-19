fc2025a5a446f8f0f3db4afa9a4826b458442931
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-07-19 13:43:42 +0200

    Done with get JSON tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,10 @@ a single test method.
 
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 # Import classes to be tested
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -40,6 +41,32 @@ class TestAccessNestedMap(unittest.TestCase):
         Tests for exceptional conditions
         """
         self.assertRaises( expected, access_nested_map, nested_map, path )
+
+class TestGetJson(unittest.TestCase):
+    """
+    class and implement the TestGetJson.test_get_json 
+    method to test that utils.get_json returns the 
+    expected result.
+    """
+
+    @parameterized.expand([
+        ["http://example.com", {"payload": True}],
+        ["http://holberton.io", {"payload": True}],
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get) -> None:
+        """
+        Test that the output of get_json is equal to test_payload.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call for testing
+        result = get_json( test_url )
+
+        mock_get.assert_called_once_with( test_url )
+        self.assertEqual( get_json(test_url), test_payload )
 
 
 if __name__ == "__main__":
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -73,3 +73,7 @@ def memoize(fn: Callable) -> Callable:
         return getattr(self, attr_name)
 
     return property(memoized)
+
+
+if __name__ == "__main__":
+    print( get_json( "http://example.com" ) )
