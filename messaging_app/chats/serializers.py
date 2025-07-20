d128dd9c5bfe746c923b1a05593b28c93c26d6d1
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-07-20 21:20:12 +0200

    Done adding serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,8 +4,9 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 
-# User Serializer
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
@@ -16,12 +17,16 @@ class UserSerializer(serializers.ModelSerializer):
             'phone_number',
             'role',
             'created_at',
+            'full_name',
         ]
 
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
-# Message Serializer
+
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
@@ -32,17 +37,26 @@ class MessageSerializer(serializers.ModelSerializer):
             'sent_at',
         ]
 
+    def validate_message_body(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Message body cannot be empty.")
+        return value
 
-# Conversation Serializer
+
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    participant_names = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = [
             'conversation_id',
             'participants',
+            'participant_names',
             'messages',
             'created_at',
         ]
+
+    def get_participant_names(self, obj):
+        return [f"{user.first_name} {user.last_name}" for user in obj.participants.all()]
